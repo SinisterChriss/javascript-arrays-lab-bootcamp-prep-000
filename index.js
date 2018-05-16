ddef7e114c54361801e83a328fac6ca8e6cd92ef
Christian Smith <th3h3ro23@gmail.com>
2018-05-16 21:08:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -31,9 +31,9 @@ function prependKitten(){
 }
 
 function removeLastKitten(){
-  return kittens.unshift();
+  return kittens.slice(0, kittens.length - 0);
 }
 
 function removeFirstKitten(){
-  return kittens.slice(0, kittens.length - 2);
+  return kittens.slice(0);
 }
