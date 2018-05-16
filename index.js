5f24a34d05468900f224fa37a25e43cc9048c92e
Christian Smith <th3h3ro23@gmail.com>
2018-05-16 20:20:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-const app = "I don't do much."
+const app = "I don't do much.";
+var kittens = ["Milo", "Otis", "Garfield"];
+var otherKitten; "Bob";
+
+function destructivelyAppendKitten(){
+  kittens.push("Ralph");
+}
+
+function destructivelyPrependKitten(){
+  kittens.unshift("Bob");
+  
+}
+
+function destructivelyRemoveLastKitten(){
+  kittens.slice(2);
+  
+}
